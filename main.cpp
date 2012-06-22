817f64ca87357581c90283322cd768b7415454f8
Misha Sakhnik <kskster@gmail.com>
2012-06-22 21:12:34 +0300

    Git init

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: morwin
+ *
+ * Created on 22 червня 2012, 21:09
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
+
