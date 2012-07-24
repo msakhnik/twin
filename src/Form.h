ec9df5b76848e3460bde3f5299d4837dca26a5a9
Misha Sakhnik <kskster@gmail.com>
2012-07-24 13:35:29 +0300

    =cForm hideAll

--- src/Form.h
+++ src/Form.h
@@ -14,60 +14,40 @@ public:
     virtual ~cForm();
 
 protected:
-    void on_button_file_clicked();
-    void on_button_folder_clicked();
-    void on_button_add_to_train_clicked();
-    void on_button_train_clicked();
-
-    void on_button_clicked();
+    void on_train();
+    void on_get_answer();
+    void on_button_choose_dialog_cliked();
 
     Gtk::VButtonBox m_button_box;
-    Gtk::Button m_button_file, m_button_add_to_train, m_button_train, m_button_folder;
+    Gtk::Button m_button_add_to_train, m_button_folder;
 
-    Gtk::VBox m_left_box, m_center_box, m_right_box;
+    Gtk::VBox m_left_box, m_center_box;
 
     Gtk::HBox m_main_box, m_hbox_button_left, m_hbox_wrap_left;
 
     Gtk::HBox m_hbox_button_center, m_hbox_wrap_center;
     Gtk::Frame m_frame_vertical_center;
 
-    Gtk::HBox m_hbox_button_right, m_hbox_wrap_right;
-    Gtk::Frame m_frame_vertical_left, m_frame_vertical_right;
+    Gtk::Frame m_frame_vertical_left;
 
     Gtk::Image m_image, m_list;
     Gtk::VPaned m_VPaned;
-    Glib::RefPtr<Gtk::ListStore> m_refListStore; //The Tree Model.
-    Gtk::TreeView m_TreeView; //The Tree View.
-    Gtk::ScrolledWindow m_ScrolledWindow;
+    Gtk::Label m_wait;
+    Gtk::Window m_modal;
+    
 
-    class ModelColumns : public Gtk::TreeModel::ColumnRecord
-    {
-    public:
-
-        ModelColumns()
-        {
-            add(m_col_num);
-            add(m_col_text);
-        }
-
-        Gtk::TreeModelColumn<Glib::ustring> m_col_num;
-        Gtk::TreeModelColumn<Glib::ustring> m_col_text;
-    };
-    ModelColumns m_Columns;
-    Gtk::TreeModel::Row m_row;
-    //        MessagesList m_MessagesList;;
 private:
-    std::string _filename;
     cFaceDetector & _face;
     cFaceRecognizer _recognizer;
     double _min_value;
-    std::string _min_filename; 
+    std::string _min_filename;
+    std::string _directory_name;
 
     void _BuildLeftBox();
     void _BuildCenterBox();
-    void _BuildRightBox();
     void _ShowOriginalImg();
-    void _AddRow();
-    void _RemoveRow();
     void _ResultProcess(std::string &);
+    void _AddTrainFiles(std::string &);
+    void _Train();
+    void _OpenModalWindow();
 };
